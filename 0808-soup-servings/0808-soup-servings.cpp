1574bb9cf823d45e9f2d8a601c6bb0160d22dc86
sristi sharma <54236156+24sristi@users.noreply.github.com>
2023-07-29 22:47:05 +0530

    Time: 15 ms (47.15%), Space: 138.3 MB (24.33%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<pair<int,int>>ops{{100,0},{75,25},{50,50},{25,75}};
+    vector<vector<double>> t;
+    double probAEmpty(double a, double b){
+        if(a<=0 && b<=0) return 0.5;
+        if(a<=0) return 1.0;
+        if(b<=0) return 0.0;
+        if(t[a][b] != -1.0)
+            return t[a][b];
+        double probablity = 0.0;
+        for(auto &p : ops){
+            double a_serve = a-p.first;
+            double b_serve = b-p.second;
+            probablity+=probAEmpty(a_serve,b_serve);
+        }
+        return t[a][b] = 0.25*probablity;
+    }
+    double soupServings(int n) {
+        if(n>=4000) return 1.0;
+        t.resize(n+1, vector<double>(n+1, -1.0));
+        return probAEmpty(n,n);
+        
+    }
+};
