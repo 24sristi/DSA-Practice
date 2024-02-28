3e4fb809c53bed997a4431b76dc340fb8ad1f341
sristi sharma <54236156+24sristi@users.noreply.github.com>
2024-02-28 15:41:21 +0530

    Time: 10 ms (24.75%), Space: 14.7 MB (31.78%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -11,15 +11,11 @@ public:
     void solve(string in, string out, vector<string> &ans) {
         if (in.length() == 0) {
             ans.push_back(out);
-            return;
+            return; 
         }
         if (isalpha(in[0])) {
-            string out1=out;
-            string out2=out;
-            out1.push_back(tolower(in[0]));
-            out2.push_back(toupper(in[0]));
-            solve(in.substr(1), out1, ans);
-            solve(in.substr(1), out2, ans);
+            solve(in.substr(1), out + static_cast<char>(tolower(in[0])), ans);
+            solve(in.substr(1), out + static_cast<char>(toupper(in[0])), ans);
         } else {
             solve(in.substr(1), out + in[0], ans);
         }
